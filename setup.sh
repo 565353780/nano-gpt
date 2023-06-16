0238c6e36faa592055f666fdd04672bc7197cdd9
chLi <565353780@qq.com>
2023-06-16 21:30:55 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install transformers datasets tiktoken wandb tqdm
+pip install transformers datasets tiktoken tqdm tensorboard
 
 pip install torch torchvision torchaudio \
-  --index-url https://download.pytorch.org/whl/cu117
+	--index-url https://download.pytorch.org/whl/cu117
