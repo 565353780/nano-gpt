9c641db5d049065df99a2f6438d03c9afc2e66b3
chLi <565353780@qq.com>
2023-06-16 21:40:27 +0800

    refine format

--- nano_gpt/Pre/openwebtext/prepare.py
+++ nano_gpt/Pre/openwebtext/prepare.py
@@ -62,7 +62,8 @@ for split, dset in tokenized.items():
     for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
         # Batch together samples for faster write
         batch = dset.shard(num_shards=total_batches,
-                           index=batch_idx, contiguous=True).with_format('numpy')
+                           index=batch_idx,
+                           contiguous=True).with_format('numpy')
         arr_batch = np.concatenate(batch['ids'])
         # Write into mmap
         arr[idx: idx + len(arr_batch)] = arr_batch
